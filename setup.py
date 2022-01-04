90dfddba10f8c3554ae89040bacc1d23d3c970bc
KOMILJONOV <komiljonovshukurullox@gmail.com>
2022-01-04 15:17:30 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="sqliteView",
+    version="0.0.1",
+    long_description=open("README.txt").read(),
+    url="http://github.com/KOMILJONOV/sqliteView",
+)
